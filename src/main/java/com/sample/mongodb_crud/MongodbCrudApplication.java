4271cd55d025b65a21f3ee3d24cc0602854ed97b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-03-02 18:20:39 +0100

    Initial commit

---
+++ src/main/java/com/sample/mongodb_crud/MongodbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sample.mongodb_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbCrudApplication.class, args);
+    }
+
+}
